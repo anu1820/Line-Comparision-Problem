c96a081c915dd60802cbdc527a34ee39c6d1806e
anu1820 <anurajpatil35@gmail.com>
2022-12-29 00:25:06 +0530

    Calling Method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Line_Comparision_Problem;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to Line Comparison Computation Program *****");
+
+           fangeometry.CalculateLength();
+        }
+    }
+}
